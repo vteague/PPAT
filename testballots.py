4e0dc42fb8c3c1e489990d8294929de9eb7ab58c
chrisculnane <chrisculnane@gmail.com>
2017-08-30 15:53:03 +1000

    Added missing updates to testballots.py

--- testballots.py
+++ testballots.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from ballots import Ballots
 from irv import IRV
 from cryptogroup import CryptoGroup
@@ -6,7 +7,13 @@ from cryptogroup import CryptoGroup
 ballots = Ballots('samplevotes')
 ballots.load()
 group = CryptoGroup()
-pk,sk = group.KeyGen()
+#pk,sk = group.KeyGen()
+#group.save_public_key(pk,'pubkey.json')
+#group.saveSecretKey(sk,'secretkey.json')
+key = {}
+key['pk']=group.loadPublicKey('pubkey.json')
+key['sk']=group.loadSecretKey('secretkey.json')
+pk,sk = group.KeyGen(key)
 
 group.make_offline_Ftable(group.Gt, pk['e'],"mytable.tbl")
 #ECtable = group.make_ECtable(group.G, pk['g'])
@@ -15,8 +22,10 @@ Ftable = group.make_full_Ftable(group.Gt, pk['e'])
 ballots.encrypt_prefs(group,pk)
 
 irv = IRV(group,pk,sk)
+start=time.time()
 irv.perform_count(ballots)
-
+end=time.time()
+print("Count:",end-start)
 #tallies = ballots.run_count(group,pk,sk)
 
 #tallycounter=1
